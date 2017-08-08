f63393c316c9fa2465e6f8029f42649a07ed65c7
TiGRoNdev <tigron.dev@gmail.com>
2017-08-08 14:05:04 +0300

    Fixed init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+sudo unlink /etc/nginx/sites-enabled/default
+
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
