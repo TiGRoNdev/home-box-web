58c8956230bca317f85f69c7f63b449c188b30e3
TiGRoNdev <tigron.dev@gmail.com>
2017-08-09 10:24:53 +0000

    Added gunicorn config

--- init.sh
+++ init.sh
@@ -4,4 +4,7 @@ sudo unlink /etc/nginx/sites-enabled/default
 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/guincorn.d/
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn hello:wsgi_app
 sudo /etc/init.d/mysql start
