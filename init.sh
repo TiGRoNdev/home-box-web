ec811af75914548479e629ab40e8b8c7755bf9bc
TiGRoNdev <tigron.dev@gmail.com>
2017-08-09 12:41:59 +0000

    First run of Gunicorn

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+git config --global user.name "TiGRoNdev"
+git config --global user.email "tigron.dev@gmail.com"
+
 sudo unlink /etc/nginx/sites-enabled/default
 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/guincorn.d/
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn hello:wsgi_app
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_app
 sudo /etc/init.d/mysql start
