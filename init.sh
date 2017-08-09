2c9280f211573f28157297225b3ecf7ad1a819e1
TiGRoNdev <tigron.dev@gmail.com>
2017-08-09 16:29:45 +0300

    Add changes to init.sh and nginx.conf

--- init.sh
+++ init.sh
@@ -4,10 +4,12 @@ git config --global user.name "TiGRoNdev"
 git config --global user.email "tigron.dev@gmail.com"
 
 sudo unlink /etc/nginx/sites-enabled/default
+sudo unlink /etc/nginx/sites-available/default
 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/guincorn.d/
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/guincorn.d/default
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_app
 sudo /etc/init.d/mysql start
