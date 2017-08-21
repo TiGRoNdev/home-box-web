f3010c82a178e9a076ce5e618f2fb2d76d40eefa
TiGRoNdev <tigron.dev@gmail.com>
2017-08-21 16:30:26 +0300

    Add urls to urls.pu

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from qa.views import test
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^login/', test, name='login'),
+	url(r'^signup/', test, name='signup'),
+	url(r'^question/([^/]+)/', test, name='login'),
+	url(r'^ask/', test, name='ask'),
+	url(r'^popular/', test, name='popular'),
+	url(r'^new/', test, name='new'),
 ]
