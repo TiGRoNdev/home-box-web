2e5e01e2a43c69f5aedd3f1089e774ce32b06877
TiGRoNdev <tigron.dev@gmail.com>
2017-08-24 18:06:18 +0000

    Added Full QuestionManager and maked all migrations for qa app

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,26 +3,30 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-#вопрос
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.get_queryset().order_by("-added_at")
+	
+	def popular(self):
+		return self.get_queryset().order_by("-rating")
+
+
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateField(auto_now_add=True)
 	rating = models.IntegerField()
 
-	author = models.OneToOneField(QuestionAuthor)
-	likes = models.ManyToManyField(User, null=True, on_delete=models.SET_NULL, related_name="question_like_user")
+	author = models.OneToOneField(User)
+	likes = models.ManyToManyField(User, related_name="question_like_user")
 	objects = QuestionManager()
 
-class QuestionManager(models.Manager):
-	
 
-#ответ
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateField(auto_now_add=True)
 	
-	question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
-	author = models.OneToOneField(AnswerAuthor)
+	question = models.ForeignKey(Question, null=False)
+	author = models.OneToOneField(User)
 
 	
