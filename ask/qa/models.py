30d385312b4568b131d339e5ebcd4af20b4fdcea
TiGRoNdev <tigron.dev@gmail.com>
2017-08-24 13:49:59 +0000

    Changed Django Models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,16 +7,16 @@ from django.contrib.auth.models import User
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateField()
+	added_at = models.DateField(auto_now_add=True)
 	rating = models.IntegerField()
 
 	author = models.OneToOneField(QuestionAuthor)
-	likes = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+	likes = models.ManyToManyField(User, null=True, on_delete=models.SET_NULL, related_name="question_like_user")
 
 #ответ
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateField()
+	added_at = models.DateField(auto_now_add=True)
 	
 	question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
 	author = models.OneToOneField(AnswerAuthor)
