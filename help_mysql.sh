4c1d7ec11767f7a684b18a0d83dad2c6448db9c4
TiGRoNdev <tigron.dev@gmail.com>
2017-08-24 11:02:37 +0000

    Added commands to start MySQL db_test

---
+++ help_mysql.sh
@@ -0,0 +1,2 @@
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS db_test" 
+mysql -uroot -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'user'"
--- init.sh
+++ init.sh
@@ -8,8 +8,8 @@ sudo unlink /etc/nginx/sites-available/default
 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-available/default
+sudo /etc/init.d/mysql start
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
 #sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_app
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
-#sudo /etc/init.d/mysql start
